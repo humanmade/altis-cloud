2fb4c347bef160ae5519fa3174bb73b076a6fda6
Joe Hoyle <joehoyle@gmail.com>
2019-03-11 16:06:26 +0700

    Require namespace.php instead

--- load.php
+++ load.php
@@ -6,6 +6,8 @@ if ( ! defined( 'WP_CACHE' ) ) {
 	define( 'WP_CACHE', true );
 }
 
+require_once __DIR__ . '/inc/namespace.php';
+
 // Load the platform as soon as WP is loaded.
 add_action( 'enable_wp_debug_mode_checks', __NAMESPACE__ . '\\bootstrap' );
 
