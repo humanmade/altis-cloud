b232facb3fd7ce57da27068bd0e3bfa3241e86fa
Joe Hoyle <joehoyle@gmail.com>
2019-04-02 18:52:01 +0700

    Update NS reference for Xray
    We are not getting db tracing currently as we are looking in the wrong namesapce for xray support

--- inc/class-db.php
+++ inc/class-db.php
@@ -2,6 +2,7 @@
 
 namespace HM\Platform\Cloud;
 
+use HM\Platform\XRay;
 use LudicrousDB;
 
 class DB extends LudicrousDB {
@@ -17,7 +18,7 @@ class DB extends LudicrousDB {
 	function query( $query ) {
 		$start = microtime( true );
 		$result = parent::query( $query );
-		if ( function_exists( __NAMESPACE__ . '\\XRay\\trace_wpdb_query' ) ) {
+		if ( function_exists( 'HM\\Platform\\XRay\\trace_wpdb_query' ) ) {
 			$host = $this->current_host ?: $this->last_connection['host'];
 			// Host gets the port number applied, which we don't want to add.
 			$host = strtok( $host, ':' );
