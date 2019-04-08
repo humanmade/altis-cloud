0381bf216a9d56505e27e838a70cb49afb7c3371
Joe Hoyle <joehoyle@gmail.com>
2019-04-08 12:19:00 +0700

    Look for wp-config-production in relation to ROOT_DIR, not ABSPATH
    ABSPATH can be somewhere totally different, such as in the case of Cavalcade-Runner's fakewp loading.

--- wp-config.php
+++ wp-config.php
@@ -5,6 +5,8 @@
  * in to a wp-config-production.php in the web root.
  */
 
-if ( file_exists( dirname( ABSPATH ) . '/wp-config-production.php' ) ) {
-	require_once dirname( ABSPATH ) . '/wp-config-production.php';
+use const HM\Platform\ROOT_DIR;
+
+if ( ROOT_DIR . '/wp-config-production.php' ) {
+	require_once ROOT_DIR . '/wp-config-production.php';
 }
