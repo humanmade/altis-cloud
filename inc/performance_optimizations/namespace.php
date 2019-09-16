56482931002d412cb12300131f5d7c60e0e62c05
Robert O'Rourke <robert@o-rourke.org>
2019-09-16 18:04:56 +0100

    remove prefetch experiment 🤦‍♂️

--- inc/performance_optimizations/namespace.php
+++ inc/performance_optimizations/namespace.php
@@ -6,8 +6,6 @@ function bootstrap() {
 	if ( strpos( $_SERVER['REQUEST_URI'], '/wp-admin/async-upload.php' ) !== false ) {
 		increase_set_time_limit_on_async_upload();
 	}
-
-	// add_action( 'wp_footer', __NAMESPACE__ . '\\prefetch_links' );
 }
 
 /**
@@ -22,28 +20,4 @@ function increase_set_time_limit_on_async_upload() {
 	if ( ini_get( 'max_execution_time' ) < 120 ) {
 		set_time_limit( 120 );
 	}
-}
-
-function prefetch_links() {
-	?>
-	<script>
-		(function(d){
-			var links = [];
-			addEventListener && d.body.addEventListener( 'click', function ( event ) {
-				if (
-					event.target.nodeName !== 'A' ||
-					links.indexOf( event.target.href ) >= 0 ||
-					event.target.href.indexOf( 'http' ) < 0
-				) {
-					return;
-				}
-				links.push( event.target.href );
-				var link = d.createElement('link');
-				link.rel = 'prefetch';
-				link.href = event.target.href;
-				d.head.appendChild(link);
-			} );
-		})(document);
-	</script>
-	<?php
 }
