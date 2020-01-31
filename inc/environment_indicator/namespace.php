c731c4b233e04e8b04a0716c0783c1b72f0dfe63
dashaluna <dashaluna@gmail.com>
2020-01-31 14:40:16 +0000

    Add env indicarot to admin bar. TODO: more CSS work

--- inc/environment_indicator/namespace.php
+++ inc/environment_indicator/namespace.php
@@ -6,60 +6,74 @@ use function Altis\get_environment_name;
 use function Altis\get_environment_type;
 
 function bootstrap() {
-	add_action( 'admin_menu', __NAMESPACE__ . '\\add_menu_page' );
+	add_action( 'wp_before_admin_bar_render', __NAMESPACE__ . '\\add_admin_bar_env_info' );
 }
 
-function add_menu_page() {
-	\add_menu_page(
-		__NAMESPACE__ . '\\get_page_title',
-		strtoupper( get_environment_type() ),
-		'manage_options',
-		'environment-indicator',
-		__NAMESPACE__ . '\\display_environment_details',
-		'dashicons-laptop',
-		1
-	);
+/**
+ * Add menu item to admin bar to display environment info:
+ * - environment type
+ * - environment name
+ * - URL to Altis dashboard for the environment
+ */
+function add_admin_bar_env_info() {
+	if ( ! is_admin_bar_showing() ) {
+		return;
+	}
 
-	\add_submenu_page(
-		'environment-indicator',
-		__NAMESPACE__ . '\\get_page_title',
-		get_environment_name(),
-		'manage_options',
-		'environment-indicator-name',
-		__NAMESPACE__ . '\\display_environment_details'
-	);
+	// Specify capabilities for which environment indicator is shown in the admin bar.
+	$capabilities[] = 'manage_options';
+
+	if ( is_multisite() ) {
+		$capabilities[] = 'manage_network_options';
+	}
+
+	$capabilities = apply_filters( 'altis_env_indicator_capabilities', $capabilities );
+	foreach ( $capabilities as $cap ) {
+		if ( ! current_user_can( $cap ) ) {
+			return;
+		}
+	}
+
+	global $wp_admin_bar;
+	// Specify admin bar menu item info and styling.
+	$indicator_colour = 'red';
+	$indicator_colour = apply_filters( 'altis_env_indicator_colour', $indicator_colour, get_environment_type() );
+
+	$indicator_text = strtoupper( get_environment_type() );
+	$indicator_text = apply_filters( 'altis_env_indicator_text', $indicator_text, get_environment_type() );
+
+	$bar_item_style = '<style>
+#wpadminbar #wp-admin-bar-altis-env-indicator>.ab-item:before {
+	content: "\f547";
 }
-
-function display_environment_details() {
-	?>
-	<div class="wrap">
-		<h1><?php echo get_page_title(); ?></h1>
-
-		<table class="form-table" role="presentation">
-			<tbody>
-			<tr>
-				<th scope="row"><?php esc_html_e( 'Environment Type', 'altis' ); ?></th>
-				<td><?php echo get_environment_type(); ?></td>
-			</tr>
-			<tr>
-				<th scope="row"><?php esc_html_e( 'Environment Name', 'altis' ); ?></th>
-				<td>
-					<?php
-					// Link to the stack on the Altis dashboard.
-					if ( get_environment_name() !== 'unknown' ) {
-						printf( '<a href="%s">%s</a>', '#', get_environment_name() );
-					} else {
-						echo get_environment_name();
-					}
-					?>
-				</td>
-			</tr>
-			</tbody>
-		</table>
-	</div>
-	<?php
+#wpadminbar #wp-admin-bar-altis-env-indicator,
+#wpadminbar #wp-admin-bar-altis-env-indicator>.ab-item:hover,
+#wpadminbar #wp-admin-bar-altis-env-indicator>.ab-item:focus,
+#wpadminbar #wp-admin-bar-altis-env-indicator>.ab-item:visited {
+	background: ' . $indicator_colour . ' ! important;
 }
+</style>';
+	$bar_item_style = apply_filters( 'altis_env_indicator_style', $bar_item_style, get_environment_type() );
 
-function get_page_title() {
-	return esc_html__( 'Environment Details', 'altis' );
+	// Add menu items to admin bar.
+	$wp_admin_bar->add_menu( [
+		'id'    => 'altis-env-indicator',
+		'title' => $bar_item_style . $indicator_text,
+	] );
+
+	// Construct Altis dashboard URL for the environment.
+	$altis_dashboard_url = '';
+	if ( defined( 'HM_ENV_REGION' ) && get_environment_type() !== 'local' ) {
+		$altis_dashboard_url = sprintf( 'https://dashboard.altis-dxp.com/#/%s/%s', esc_url( HM_ENV_REGION ), esc_url( get_environment_name() ) );
+	}
+
+	$wp_admin_bar->add_menu( [
+		'id'     => 'altis-env-stack-url',
+		'title'  => sprintf( __( 'Altis dashboard: %s', 'altis' ), esc_html( get_environment_name() ) ),
+		'parent' => 'altis-env-indicator',
+		'href'   => $altis_dashboard_url,
+		'meta'   => [
+			'target' => '__blank',
+		],
+	] );
 }
