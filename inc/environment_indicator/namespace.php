ea19e6621a648ef0b8dea53c92bcc360ecae49de
dashaluna <dashaluna@gmail.com>
2020-02-11 10:55:05 +0000

    Refactor code - add constant, use if instead of ternary operator, urlencode query params

--- inc/environment_indicator/namespace.php
+++ inc/environment_indicator/namespace.php
@@ -6,6 +6,8 @@ use function Altis\get_environment_name;
 use function Altis\get_environment_type;
 use WP_Admin_Bar;
 
+const SUPPORT_URL = 'https://dashboard.altis-dxp.com/#/support/new';
+
 function bootstrap() {
 	add_action( 'admin_bar_init', __NAMESPACE__ . '\\enqueue_admin_scripts' );
 	// Allow other modules to add sub-menu items to Altis admin bar menu first.
@@ -100,15 +102,17 @@ function add_admin_bar_support_ticket_link( WP_Admin_Bar $wp_admin_bar ) {
 	}
 
 	$env_name    = get_environment_name();
-	$support_url = 'https://dashboard.altis-dxp.com/#/support/new';
-	$support_url = 'unknown' === $env_name ? $support_url : "{$support_url}?applications[]={$env_name}";
+	$support_url = SUPPORT_URL;
+	if ( 'unknown' !== $env_name ) {
+		$support_url .= sprintf( '?applications[]=%s', urlencode( $env_name ) );
+	}
 
 	// Add support ticket URL as a sub-menu item to the Altis logo menu in the admin bar.
 	$wp_admin_bar->add_menu( [
 		'parent' => 'altis',
 		'id'     => 'altis-support-ticket',
 		'title'  => __( 'Open Support Ticket', 'altis' ) . ' <span class="dashicons-before dashicons-external"></span>',
-		'href'   => esc_url( $support_url ),
+		'href'   => $support_url,
 		'meta'   => [
 			'target' => '_blank',
 		],
