c5c00143998bec88526aaa790a00f5b3ce0b4343
Abhishek Kaushik <abhishek@humanmade.com>
2020-04-24 15:24:15 +0530

    Add Instance Healthcheck
    Signed-off-by: Abhishek Kaushik <abhishek@humanmade.com>

--- inc/healthcheck/class-cli-command.php
+++ inc/healthcheck/class-cli-command.php
@@ -7,6 +7,15 @@ use WP_CLI;
 use WP_CLI_Command;
 
 class CLI_Command extends WP_CLI_Command {
+
+	/**
+	 * Run health checks.
+	 *
+	 * @param array $args
+	 * @param array $args_assoc
+	 *
+	 * @return void
+	 */
 	function run( array $args, array $args_assoc ) {
 		$args_assoc = wp_parse_args( $args_assoc, [
 			'format' => 'table',
@@ -15,6 +24,39 @@ class CLI_Command extends WP_CLI_Command {
 
 		$data = [];
 		$passed = true;
+		foreach ( $checks as $check => $result ) {
+			if ( is_wp_error( $result ) ) {
+				$passed = false;
+			}
+			$data[] = [
+				'check' => $check,
+				'status' => is_wp_error( $result ) ? cli\Colors::colorize( '%R' . $result->get_error_message() . '%n' ) : cli\Colors::colorize( '%GPassed%n' ),
+			];
+		}
+		WP_CLI\utils\format_items( $args_assoc['format'], $data, [ 'check', 'status' ] );
+
+		// Send exit code.
+		WP_CLI::halt( $passed ? 0 : 1 );
+	}
+
+	/**
+	 * Run instance health checks.
+	 *
+	 * @param array $args
+	 * @param array $args_assoc
+	 *
+	 * @return void
+	 */
+	function instance( array $args, array $args_assoc ) {
+		$args_assoc = wp_parse_args( $args_assoc, [
+			'format' => 'table',
+		]);
+
+		$checks = run_instance_checks();
+
+		$data   = [];
+		$passed = true;
+
 		foreach ( $checks as $check => $result ) {
 			if ( is_wp_error( $result ) ) {
 				$passed = false;
