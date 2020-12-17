3e3806c49404a515751c33c22ca02d253d456bb7
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2020-12-16 18:15:32 -0600

    add custom monolog formatter class to format the messages using msgpack

--- inc/fluent-bit/namespace.php
+++ inc/fluent-bit/namespace.php
@@ -2,29 +2,38 @@
 
 namespace Altis\Cloud\FluentBit;
 
-// use Monolog\Formatter\MsgPackFormatter;
+use Altis\Cloud\FluentBit\Formatter;
 use Monolog\Handler\SocketHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
 function get_logger( string $tag_name ) {
+	// TODO how do I need to name the file/class to be autoloaded?
+	require_once __DIR__ . '/class-formatter.php';
+
 	static $loggers = [];
 
+	error_log('getting');
 	if ( $loggers[ $tag_name ] ) {
+		error_log('defined');
 		return $loggers[ $tag_name ];
 	}
 
+	error_log('creating logger');
 	$logger = new Logger( $tag_name );
+
+	// Else we log locally
+	$stream = new StreamHandler( '/tmp/logpipe', Logger::DEBUG );
+	$logger->pushHandler( $stream );
 
 	// Use Fluent Bit if it's available
 	if ( defined( 'FLUENT_HOST' ) && defined( 'FLUENT_PORT' ) ) {
 		$socket = new SocketHandler( FLUENT_HOST . ':' . FLUENT_PORT, Logger::DEBUG );
+		$socket->setFormatter( new Formatter() );
 		$logger->pushHandler( $socket );
 	} else {
-		// Else we log locally
-		$stream = new StreamHandler( '/tmp/logpipe', Logger::DEBUG );
-		$logger->pushHandler( $stream );
 	}
+
 
 	$loggers[ $tag_name ] = $logger;
 	return $loggers[ $tag_name ];
