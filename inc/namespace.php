7442ef871c201ad284bdbd61be37f79c90e1f2f0
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2020-12-17 15:46:00 -0600

    create log_to_cloud function to serve as mediator betweent Fluent Bit or CloudWatch API

--- inc/namespace.php
+++ inc/namespace.php
@@ -8,6 +8,7 @@
 namespace Altis\Cloud;
 
 use Altis;
+use Altis\Cloud\CloudWatch_Logs;
 use Altis\Cloud\Fluent_Bit;
 use Aws\CloudFront\CloudFrontClient;
 use Aws\Credentials;
@@ -758,21 +759,21 @@ function on_request_stats( TransferStats $stats ) {
  * This will cause a remote request to the metadata service when the cache is empty.
  *
  * @return array $array(
- *    accountId: string,
- *    architecture: string,
- *    availabilityZone: string,
- *    billingProducts: string,
- *    devpayProductCodes: string,
- *    marketplaceProductCodes: string,
- *    imageId: string,
- *    instanceId: string,
- *    instanceType: string,
- *    kernelId: string,
- *    pendingTime: string,
- *    privateIp: string,
- *    ramdiskId: string,
- *    region: string,
- *    version: string,
+ *	  accountId: string,
+ *	  architecture: string,
+ *	  availabilityZone: string,
+ *	  billingProducts: string,
+ *	  devpayProductCodes: string,
+ *	  marketplaceProductCodes: string,
+ *	  imageId: string,
+ *	  instanceId: string,
+ *	  instanceType: string,
+ *	  kernelId: string,
+ *	  pendingTime: string,
+ *	  privateIp: string,
+ *	  ramdiskId: string,
+ *	  region: string,
+ *	  version: string,
  * )
  */
 function get_ec2_instance_metadata() : array {
@@ -869,13 +870,13 @@ function get_cloudfront_client() : CloudFrontClient {
  * @see https://www.altis-dxp.com/resources/docs/cloud/cdn-purge/
  *
  * @param array $paths_patterns A list of the paths that you want to invalidate.
- *                              The path is relative to the CDN host, A leading / is optional.
- *                              e.g  for http://altis-dxp.com/images/image2.jpg
- *                              specify images/image2.jpg or /images/image2.jpg
+ *								The path is relative to the CDN host, A leading / is optional.
+ *								e.g  for http://altis-dxp.com/images/image2.jpg
+ *								specify images/image2.jpg or /images/image2.jpg
  *
- *                              You can also invalidate multiple files simultaneously by using the * wildcard.
- *                              The *, which replaces 0 or more characters, must be the last character in the invalidation path.
- *                              e.g /images/* - will invalidate all files in a directory.
+ *								You can also invalidate multiple files simultaneously by using the * wildcard.
+ *								The *, which replaces 0 or more characters, must be the last character in the invalidation path.
+ *								e.g /images/* - will invalidate all files in a directory.
  *
  * @return bool Returns true if invalidation successfully created, false on failure.
  */
@@ -927,9 +928,9 @@ function purge_cdn_paths( array $paths_patterns ) : bool {
 
 	try {
 		$client->createInvalidation( [
-			'DistributionId'    => $distribution_id,
+			'DistributionId'	=> $distribution_id,
 			'InvalidationBatch' => [
-				'Paths'           => [
+				'Paths'			  => [
 					'Items'    => $paths_patterns,
 					'Quantity' => count( $paths_patterns ),
 				],
@@ -958,4 +959,28 @@ function set_wp_debug_constants() : void {
 		return;
 	}
 	define( 'WP_DEBUG_DISPLAY', false );
+}
+
+
+function log_to_cloud( string $log_group, string $log_stream, string $message, string $level = 'info' ) : bool {
+	if ( ! Fluent_Bit\enabled() ) {
+		$result = CloudWatch_Logs\send_events_to_stream(
+			[
+				[
+					'timestamp' => time() * 1000,
+					'message' => $message,
+				],
+			],
+			Altis\get_environment_name() . '/' . $log_group,
+			$log_stream
+		);
+
+		return $result;
+	}
+
+	$fluent_tag = sprintf( 'app.%s.%s', $log_group, $log_stream );
+	$logger = Fluent_Bit\get_logger( $fluent_tag );
+	$logger->$level( $message );
+
+	return true;
 }
