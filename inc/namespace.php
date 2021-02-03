fbca46320aa7644a245f494819c15d54d4dd1689
Nathaniel <314009+nathanielks@users.noreply.github.com>
2021-02-03 14:07:17 -0600

    Update inc/namespace.php
    Co-authored-by: Ryan McCue <me@ryanmccue.info>

--- inc/namespace.php
+++ inc/namespace.php
@@ -935,7 +935,7 @@ function purge_cdn_paths( array $paths_patterns ) : bool {
 
 	try {
 		$client->createInvalidation( [
-			'DistributionId'    => $distribution_id,
+			'DistributionId' => $distribution_id,
 			'InvalidationBatch' => [
 				'Paths' => [
 					'Items' => $paths_patterns,
