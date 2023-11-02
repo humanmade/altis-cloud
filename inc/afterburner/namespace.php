8036a8c11d218fbd021a229b7e4105cb5ae53b6f
Joe Hoyle <joehoyle@gmail.com>
2023-11-02 20:29:51 +0100

    Add Afterburner panel to QM

---
+++ inc/afterburner/namespace.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Altis\Cloud\Afterburner;
+
+function bootstrap() : void {
+	add_filter( 'qm/outputter/html', __NAMESPACE__ . '\\register_qm_output_html' );
+}
+
+/**
+ * Register the HTML outputter for the Xray panel
+ *
+ * @param array $output
+ * @return array
+ */
+function register_qm_output_html( array $output ) : array {
+	require_once __DIR__ . '/class-qm-output-html.php';
+	require_once __DIR__ . '/class-qm-collector.php';
+
+	$output['afterburner'] = new QM_Output_Html( new QM_Collector() );
+
+	return $output;
+}
