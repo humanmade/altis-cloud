4546d36c4ef3435f7939bf0a883fea807221cc83
Joe Hoyle <joehoyle@gmail.com>
2023-11-02 20:29:51 +0100

    Add Afterburner panel to QM

---
+++ inc/afterburner/class-qm-collector.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Altis Afterburner data collector for the Query Monitor plugin.
+ *
+ */
+
+namespace Altis\Cloud\Afterburner;
+
+
+/**
+ * Altis Config Qm Collector.
+ *
+ */
+class QM_Collector extends \QM_Collector {
+
+	/**
+	 * Collector ID.
+	 *
+	 * @var string
+	 */
+	public $id = 'afterburner';
+
+}
