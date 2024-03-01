df538d78a6a03f4342eb664c52f2a258b6fd8cc5
Joe Hoyle <joehoyle@gmail.com>
2024-03-01 09:39:43 +0200

    phpcs

--- inc/afterburner/class-qm-collector.php
+++ inc/afterburner/class-qm-collector.php
@@ -1,15 +1,12 @@
 <?php
 /**
  * Altis Afterburner data collector for the Query Monitor plugin.
- *
  */
 
 namespace Altis\Cloud\Afterburner;
 
-
 /**
  * Altis Config Qm Collector.
- *
  */
 class QM_Collector extends \QM_Collector {
 
